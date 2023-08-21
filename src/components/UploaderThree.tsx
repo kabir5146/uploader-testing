e17f430d2dd7f64b5b575a0edfb4492317f3e5dd
kabir5146 <fl.kabirhossain@gmail.com>
2023-08-21 19:00:40 +0600

    making uploader three

--- src/components/UploaderThree.tsx
+++ src/components/UploaderThree.tsx
@@ -66,66 +66,62 @@ const UploaderThree = () => {
         <div className="w-full flex flex-col justify-center items-center gap-5 mt-8">
           {file && (
             <>
+              {" "}
+              <div className=" flex flex-col items-center justify-center rounded bg-white relative  ">
+                <h2 className="text-[#7F9AFB] text-2xl font-medium mb-5">
+                  File Uploader JavaScript
+                </h2>
+                <div className="w-[420px] h-[200px] rounded-md border-2 border-dashed border-[#7F9AFB] text-[#A0B2E0] flex flex-col items-center justify-center relative">
+                  <i className="fa-solid fa-cloud-arrow-up text-5xl p-1 text-[#7F9AFB]"></i>
+                  <p className="font-medium text-xl leading-8 capitalize mt-3">
+                    browse file to upload
+                  </p>
+                  <input
+                    type="file"
+                    multiple
+                    name=""
+                    id=""
+                    className="h-full w-full opacity-0 absolute"
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                      const files = e.target.files;
+                      if (files) {
+                        const fileArr = Object.values(files);
+                        // console.log(' Home ~ fileArr:', fileArr);
+                        if (file.length <= 3) {
+                          setFile((prevValue) =>
+                            prevValue
+                              ? [...prevValue, ...fileArr]
+                              : [...fileArr]
+                          );
+                        }
+                      }
+                    }}
+                  />
+                </div>
+              </div>
               {file.map((item: File, i: number) => {
+                const progressPercentage = 50;
+
                 return (
                   <div
                     key={i}
-                    className="w-60 h-80 rounded bg-white relative  "
+                    className="w-[420px] h-16 flex flex-row items-center justify-between rounded bg-[#EDF2FF] relative px-5"
                   >
-                    <Image
-                      src={URL.createObjectURL(item)}
-                      alt=""
-                      fill
-                      className="object-contain z-10 "
-                    />
-                    <button
-                      type="button"
-                      onClick={() => {
-                        const newFileArr = file.filter(
-                          (element) =>
-                            item.lastModified !== element.lastModified
-                        );
-                        setFile(newFileArr);
-                      }}
-                      className="absolute z-20 px-2 py-1 rounded-md bg-green-200 text-black top-2 right-2 capitalize "
-                    >
-                      {" "}
-                      remove{" "}
-                    </button>
-                    <button
-                      type="button"
-                      onClick={() => {
-                        setEditFileIndex(i);
-                        editInputRef.current?.click();
-                      }}
-                      className="absolute z-20 px-2 py-1 rounded-md bg-[#FE0A00] text-white top-2 left-2 capitalize "
-                    >
-                      {" "}
-                      edit{" "}
-                    </button>
+                    <i className="fa-regular fa-file-lines text-[#7F9AFB] text-3xl"></i>
+                    <div className="flex flex-col ml-3 w-[90%]">
+                      <div className="w-full flex flex-row items-center justify-between text-[#000000] text-xs leading-4">
+                        <p className="">{item.name}</p>
+                        <p className="">{progressPercentage}%</p>
+                      </div>
+                      <div className="w-full h-2 bg-white mt-1 rounded-md">
+                        <div
+                          className={`h-full w-1/2 bg-[#7F9AFB] rounded-md ${progressPercentage}`}
+                        ></div>
+                      </div>
+                    </div>
                   </div>
                 );
               })}
-              <div className="w-60 h-80 flex flex-col items-center justify-center shadow-lg shadow-purple-200 rounded bg-purple-50 relative  ">
-                <input
-                  type="file"
-                  multiple
-                  className="opacity-0 absolute w-full h-full z-10"
-                  onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                    const files = e.target.files;
-                    if (files) {
-                      const fileArr = Object.values(files);
-                      // console.log(' Home ~ fileArr:', fileArr);
-                      if (file.length <= 3) {
-                        setFile((prevValue) =>
-                          prevValue ? [...prevValue, ...fileArr] : [...fileArr]
-                        );
-                      }
-                    }
-                  }}
-                />
-                <h1 className="text-xl leading-8 font-bold">Upload More</h1>
-              </div>
             </>
           )}
         </div>
